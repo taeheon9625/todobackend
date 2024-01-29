ec88faec809c2ea47893a08bf93103a799190bf8
adam <itstudy@kakao.com>
2024-01-29 17:34:10 +0900

    메시지

---
+++ todoapplication/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True)
+    userid = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    done = models.BooleanField(default=False)
+    regdate = models.DateTimeField(auto_now_add=True)
+    moddate = models.DateTimeField(null=True)
